385c5fcb6673756051484cefd7f82000f59e56b5
Alistar542 <alistarfz@gmail.com>
2017-09-04 13:43:11 +0530

    next

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("page-top").style.display = "block";
+}
+
+
 $(document).ready(function(){
     // Activate Carousel
     $("#my-car").carousel();
